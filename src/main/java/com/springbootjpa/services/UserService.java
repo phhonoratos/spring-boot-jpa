195414cf7cd70b3a9793b70296b133e29ce9e9b5
Paulo Honorato <paulohonoratos@hotmail.com>
2021-06-11 20:56:19 -0300

    Service layer, component registration

---
+++ src/main/java/com/springbootjpa/services/UserService.java
@@ -0,0 +1,26 @@
+package com.springbootjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.springbootjpa.entities.User;
+import com.springbootjpa.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+    
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
