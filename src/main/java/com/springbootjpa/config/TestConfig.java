61886c7c0620b2b5e0a65498f70204883d432d88
Paulo Honorato <paulohonoratos@hotmail.com>
2021-06-20 17:14:45 -0300

    Payment, one-to-one association

--- src/main/java/com/springbootjpa/config/TestConfig.java
+++ src/main/java/com/springbootjpa/config/TestConfig.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import com.springbootjpa.entities.Category;
 import com.springbootjpa.entities.Order;
 import com.springbootjpa.entities.OrderItem;
+import com.springbootjpa.entities.Payment;
 import com.springbootjpa.entities.Product;
 import com.springbootjpa.entities.User;
 import com.springbootjpa.entities.enums.OrderStatus;
@@ -76,6 +77,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 
 }
