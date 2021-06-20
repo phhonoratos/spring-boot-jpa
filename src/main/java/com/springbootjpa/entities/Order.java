b6e960eae4c9615d4a4718bb03c86065a2f73e36
Paulo Honorato <paulohonoratos@hotmail.com>
2021-06-20 17:40:07 -0300

    Subtotal & Total methods

--- src/main/java/com/springbootjpa/entities/Order.java
+++ src/main/java/com/springbootjpa/entities/Order.java
@@ -99,6 +99,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal() {
+        double soma = 0;
+        for(OrderItem x : items) {
+            soma += x.getSubTotal();
+        }
+        return soma;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
